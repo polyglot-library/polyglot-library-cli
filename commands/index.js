f49a9df1898aecac39fc785bee95a198ceac1d23
Wallysson Nunes <wally.nm@gmail.com>
2020-03-28 02:55:21 -0300

    Added initial CLI files with basic integrations

---
+++ commands/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Text, useApp, Box } from 'ink';
+import TextInput from 'ink-text-input';
+
+
+// Hello world command
+const Hello = ({ name }) => {
+  const { exit } = useApp();
+  const [input, setInput] = useState('');
+
+  const onChange = (data) => {
+    setInput(data);
+  };
+  const onSubmit = () => {
+    console.info(input);
+    exit();
+  };
+
+  return (
+    <>
+      <Box>
+        <Box marginRight={1}>
+          <Text>Hello, please type your name:</Text>
+        </Box>
+        <TextInput
+          value={input}
+          onChange={onChange}
+          onSubmit={onSubmit}
+        />
+      </Box>
+    </>
+  );
+};
+
+Hello.propTypes = {
+  // / Name of the person to greet
+  name: PropTypes.string.isRequired,
+};
+
+export default Hello;
